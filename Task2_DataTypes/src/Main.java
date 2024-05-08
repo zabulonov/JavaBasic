224f8549bf7858edf68532d970526ecbe2982b9b
Alexey <zabulonov444@yandex.ru>
2024-05-08 12:49:07 +0300

    Add Task2 Data Types

---
+++ Task2_DataTypes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 3.141592654;
+        byte b = 127;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
